c2f4b2907f02e7e1a49fb143477bd817ba038a91
Farhan Syed <Nearhan@Farhans-MacBook-Pro.local>
2012-06-21 10:56:54 -0400

    added this stuff

--- python 2/koans/triangle.py
+++ python 2/koans/triangle.py
@@ -18,9 +18,15 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0 or a + b <= c or a + c <= b:
+        raise TriangleError
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
-
+    if a == b and b == c:
+	    return 'equilateral'
+    elif a == b or b == c or a == c:
+	    return 'isosceles'
+    else:
+	    return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(StandardError):
